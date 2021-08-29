6665478fae0bc0a7d653e9bcc5e03d31d461721d
Niquelle <niqjohnson1@gmail.com>
2021-08-29 16:29:34 -0400

    added photos

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Images';
+  image1 = 'https://noodlepros.ghost.io/content/images/wordpress/2017/12/coding-or-foreign-language-scaled.jpg';
+  image2 = 'https://m.media-amazon.com/images/M/MV5BOWEzNDAxYmEtYWU0Zi00ZjZjLTkxY2QtMGY1MjY5ZjVhNDdjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg';
+  image3 = 'https://cf.ltkcdn.net/cleaning/images/orig/202845-1737x1414-chores.jpg';
 
   constructor() { }
 
